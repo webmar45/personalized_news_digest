ef99655c69e7506a97689699f5776fdcd02beae6
webmar45 <webmar45@gmail.com>
2026-01-05 02:18:23 +0530

    Ready for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,5 +5,12 @@ const API = axios.create({
   // Use relative path for production, localhost for dev
   baseURL: '/api', 
 });
-
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  console.log("Sending Token:", token); // CHECK THIS IN YOUR CONSOLE
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 export default API;
